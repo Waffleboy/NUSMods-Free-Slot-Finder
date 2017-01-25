b0d2dbf6abfaecee96fd62a20d42c0a73d28e0c6
waffleboy <thiru1337@gmail.com>
2017-01-26 00:46:33 +0800

    initial commit

---
+++ timetable.js
@@ -0,0 +1,25 @@
+function Timetable(){
+    this.timetable = [];
+
+    this.populateTimetableWithURL = function(url){
+        /* initiate API call to get back data of form:
+    
+        {module:
+            {type: "lecture",
+            timestart: 1900,
+            timeend: 2200,
+            Venue:X,
+            WeekText:every week,
+            DayText
+            }}
+        */
+    }
+
+    this.queryAPI = function(url){
+        // returns the hash
+    }
+
+    this.findFreeSlots = function(){
+
+    }
+}
